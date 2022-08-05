49ef6f13f8464dd37a66a0f5cc5c2f018563a6e3
sfgu <sfgu@hillinsight.com>
2022-08-05 16:42:51 +0800

    refactor(shared/number): update numberWithCommas

--- packages/shared/src/number.ts
+++ packages/shared/src/number.ts
@@ -1,9 +1,12 @@
 /**
  * 千位分隔符
- * @param num 数字
+ * @param x 数字
  * @returns
  */
 // from https://stackoverflow.com/a/2901298/
-export const numberWithCommas = (num: number) => {
-  return `${num}`.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
+export const numberWithCommas = (x: number) => {
+  // return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',') // Safari not support lookbehind
+  const parts = x.toString().split('.')
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  return parts.join('.')
 }
